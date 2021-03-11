6e241b2d4628878fdb51431c7e09f0344ea9d139
Rich Shakespeare <richshakespeare@protonmail.com>
2021-03-11 09:32:50 +0000

    log invalid emails signing in

--- app/app.rb
+++ app/app.rb
@@ -406,9 +406,12 @@ module ActivateAdmin
         account = Account.where('email ilike ?', params[:email]).first
       elsif mongoid?
         account = Account.find_by(email: /^#{Regexp.escape(params[:email])}$/i)
+        unless account.present?
+          Padrino.logger.warn("Someone tried to log in with a non-existant email: #{params[:email]}")
+        end
       end
       account.reset_password!
-      flash[:notice] = "Instructions have been sent to #{account.email}"
+      flash[:notice] = "If an account with the email #{account.email} exists, you'll receive instructions on what to do next"
       redirect url(:login)
     end
 
